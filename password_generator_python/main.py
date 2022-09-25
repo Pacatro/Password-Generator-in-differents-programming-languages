e6e98102648ba273da8826f4a045f8177e8d68bb
Pacatro <paco.algar.15@gmail.com>
2022-09-25 16:19:57 +0200

    Password Generator in c++, python and javascript using html and css

--- password_generator_python/main.py
+++ password_generator_python/main.py
@@ -1,8 +1,15 @@
 import random
 import os
+import platform
 from time import sleep
 from io import open
 
+def commad():
+    system_os = platform.system()
+    if system_os == 'Linux':
+        return "clear"
+
+    return "cls"
 
 def generate_password(long):
     words = "abcdefghijklmnopqrstuvwxyz"
@@ -17,10 +24,36 @@ def generate_password(long):
     
     return password
 
-
-def generate_file(password, file_name, reminder):
+def print_file():
     try:
-        file = open(file_name, "a")
+        file = open("passwords.txt", "r")
+        for line in file:
+            print(line)
+    
+    except:
+        print("\nERROR, CAN'T READ THE FILE")
+        input("\nPress 'Enter' to continue")
+        os.system(commad())
+
+def reminder_in_file(reminder):
+    try:
+        file = open("passwords.txt", "r")
+        for line in file:
+            if reminder in line:
+                file.close()
+                return True
+            
+        file.close()
+        return False
+    
+    except:
+        print("\nERROR, CAN'T READ THE FILE")
+        input("\nPress 'Enter' to continue")
+        os.system(commad())
+
+def write_file(password, reminder):
+    try:
+        file = open("passwords.txt", "a")
         file.write(reminder + " --> " + password + "\n")
         file.close()
         
@@ -29,24 +62,22 @@ def generate_file(password, file_name, reminder):
     except:
         print("\nERROR, CAN'T CREATE THE FILE")
         input("\nPress 'Enter' to continue")
-        os.system("cls")
-
+        os.system(commad())
 
 def exit_program(condition):
     print("\nExit...")
     sleep(0.5)
     return not condition
 
-    
 def menu():
-    os.system("cls")
+    os.system(commad())
     print("\n-----Welcome to the Password Generator!-----\n")
     print("What do you wanna do?\n")
     
     print("Generate password (G)")
+    print("Read file (R)")
     print("Exit (Press any key)")
-
-    
+ 
 def main():
     condition = True
     
@@ -73,16 +104,24 @@ def main():
             
             if choose == 'Y':
                 reminder = input("\nWrite something for remember your password: ")
-                file_name = input("\nFile name (example: file.txt): ")
 
-                generate_file(password, file_name, reminder)
+                if reminder_in_file(reminder):
+                    print("\nERROR, The reminder or password already exists in the file.")
+
+                else:
+                    write_file(password, reminder)
 
             input("\nPress 'Enter' to continue")
-            os.system("cls")
+            os.system(commad())
+
+        elif option == 'R':
+            print("\n-----Passwords.txt-----\n")
+            print_file()
+            input("\nPress 'Enter' to continue")
+            os.system(commad())
            
         else:
             condition = exit_program(True)
                 
-    
 if __name__ == '__main__':
     main()
